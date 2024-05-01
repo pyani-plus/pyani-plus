b1eeb4a26abaa0cc9d10bf43c0e6d52b33010bc3
kiepczi <angelika.kiepas@strath.ac.uk>
2024-05-01 16:16:27 +0100

    Fix test description

--- tests/snakemake/test_snakemake_fastani_workflow.py
+++ tests/snakemake/test_snakemake_fastani_workflow.py
@@ -28,23 +28,16 @@ These tests are intended to be run from the repository root using:
 make test
 """
 
-import shutil  # We need this for filesystem operations
-
-# Required to support pytest automated testing
+import shutil
 import pytest
 
-# We're testing the workflow, as called through the pyani_plus
-# wrapper code, so import only that module
+
 from pyani_plus.snakemake import fastani
 
 
 @pytest.fixture
 def config_fastani_args(fastani_targets_outdir, input_genomes_small):
-    """Configuration settings for testing snakemake filter rule.
-
-    We take the output directories for the MUMmer filter output and the
-    small set of input genomes as arguments.
-    """
+    """Configuration settings for testing snakemake fastANI rule."""
     return {
         "outdir": fastani_targets_outdir,
         "indir": input_genomes_small,
@@ -58,16 +51,10 @@ def config_fastani_args(fastani_targets_outdir, input_genomes_small):
 def test_snakemake_rule_fastani(
     fastani_targets, fastani_targets_outdir, config_fastani_args
 ):
-    """Test nucmer filter snakemake wrapper
+    """Test fastANI snakemake wrapper
 
-    Checks that the filter rule in the ANIm snakemake wrapper gives the
+    Checks that the fastANI rule in the fastANI snakemake wrapper gives the
     expected output.
-
-    If the output directory exists (i.e. the make clean_tests rule has not
-    been run), the tests will automatically pass as snakemake will not
-    attempt to re-run the rule. That would prevent us from seeing any
-    introduced bugs, so we force re-running the rule by deleting the
-    output directory before running the tests.
     """
     # Remove the output directory to force re-running the snakemake rule
     shutil.rmtree(fastani_targets_outdir, ignore_errors=True)

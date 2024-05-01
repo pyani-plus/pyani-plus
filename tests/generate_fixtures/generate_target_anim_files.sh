40641158fa3624c85bfee5476c491c63adb45a18
Leighton Pritchard <leightonpritchard@mac.com>
2024-05-01 16:47:46 +0100

    update format of fixture generation script
    - make shebang more portable
    - make variables uppercase
    - expand comments

--- tests/generate_fixtures/generate_target_anim_files.sh
+++ tests/generate_fixtures/generate_target_anim_files.sh
@@ -1,48 +1,52 @@
-#!/bin/bash
-
+#!/bin/env bash
+#
 # This bash script generates target files for anim comparisions.
 # Genomes are compared in both directions (forward and reverse)
-# using nucmer and delta-filter with --mum parameter (nucmer)
-# and -1 parameter (delta-filter). 
+# using nucmer and delta-filter.
+#
+# nucmer runs with the --mum parameter to find initial maximal
+# unique matches.
+# delta-filter runs with the -1 parameter to filter only 1-to-1
+# matches
 
 # Paths to directories (eg, input sequences, delta and filter)
-input_dir=../fixtures/sequences
-delta_dir=../fixtures/anim/targets/delta
-filter_dir=../fixtures/anim/targets/filter
+INPUT_DIR=../fixtures/sequences
+DELTA_DIR=../fixtures/anim/targets/delta
+FILTER_DIR=../fixtures/anim/targets/filter
 
 #Running comparisions
-nucmer -p $delta_dir/NC_002696_vs_NC_010338 --mum $input_dir/NC_002696.fna $input_dir/NC_010338.fna
-delta-filter -1 $delta_dir/NC_002696_vs_NC_010338.delta > $filter_dir/NC_002696_vs_NC_010338.filter
+nucmer -p $DELTA_DIR/NC_002696_vs_NC_010338 --mum $INPUT_DIR/NC_002696.fna $INPUT_DIR/NC_010338.fna
+delta-filter -1 $DELTA_DIR/NC_002696_vs_NC_010338.delta > $FILTER_DIR/NC_002696_vs_NC_010338.filter
 
-nucmer -p $delta_dir/NC_002696_vs_NC_011916 --mum $input_dir/NC_002696.fna $input_dir/NC_011916.fna
-delta-filter -1 $delta_dir/NC_002696_vs_NC_011916.delta > $filter_dir/NC_002696_vs_NC_011916.filter 
+nucmer -p $DELTA_DIR/NC_002696_vs_NC_011916 --mum $INPUT_DIR/NC_002696.fna $INPUT_DIR/NC_011916.fna
+delta-filter -1 $DELTA_DIR/NC_002696_vs_NC_011916.delta > $FILTER_DIR/NC_002696_vs_NC_011916.filter 
 
-nucmer -p $delta_dir/NC_002696_vs_NC_014100 --mum $input_dir/NC_002696.fna $input_dir/NC_014100.fna
-delta-filter -1 $delta_dir/NC_002696_vs_NC_014100.delta > $filter_dir/NC_002696_vs_NC_014100.filter 
+nucmer -p $DELTA_DIR/NC_002696_vs_NC_014100 --mum $INPUT_DIR/NC_002696.fna $INPUT_DIR/NC_014100.fna
+delta-filter -1 $DELTA_DIR/NC_002696_vs_NC_014100.delta > $FILTER_DIR/NC_002696_vs_NC_014100.filter 
 
-nucmer -p $delta_dir/NC_010338_vs_NC_002696 --mum $input_dir/NC_010338.fna $input_dir/NC_002696.fna
-delta-filter -1 $delta_dir/NC_010338_vs_NC_002696.delta > $filter_dir/NC_010338_vs_NC_002696.filter 
+nucmer -p $DELTA_DIR/NC_010338_vs_NC_002696 --mum $INPUT_DIR/NC_010338.fna $INPUT_DIR/NC_002696.fna
+delta-filter -1 $DELTA_DIR/NC_010338_vs_NC_002696.delta > $FILTER_DIR/NC_010338_vs_NC_002696.filter 
 
-nucmer -p $delta_dir/NC_010338_vs_NC_011916 --mum $input_dir/NC_010338.fna $input_dir/NC_011916.fna
-delta-filter -1 $delta_dir/NC_010338_vs_NC_011916.delta > $filter_dir/NC_010338_vs_NC_011916.filter 
+nucmer -p $DELTA_DIR/NC_010338_vs_NC_011916 --mum $INPUT_DIR/NC_010338.fna $INPUT_DIR/NC_011916.fna
+delta-filter -1 $DELTA_DIR/NC_010338_vs_NC_011916.delta > $FILTER_DIR/NC_010338_vs_NC_011916.filter 
 
-nucmer -p $delta_dir/NC_010338_vs_NC_014100 --mum $input_dir/NC_010338.fna $input_dir/NC_014100.fna
-delta-filter -1 $delta_dir/NC_010338_vs_NC_014100.delta > $filter_dir/NC_010338_vs_NC_014100.filter
+nucmer -p $DELTA_DIR/NC_010338_vs_NC_014100 --mum $INPUT_DIR/NC_010338.fna $INPUT_DIR/NC_014100.fna
+delta-filter -1 $DELTA_DIR/NC_010338_vs_NC_014100.delta > $FILTER_DIR/NC_010338_vs_NC_014100.filter
 
-nucmer -p $delta_dir/NC_011916_vs_NC_002696 --mum $input_dir/NC_011916.fna $input_dir/NC_002696.fna
-delta-filter -1 $delta_dir/NC_011916_vs_NC_002696.delta > $filter_dir/NC_011916_vs_NC_002696.filter
+nucmer -p $DELTA_DIR/NC_011916_vs_NC_002696 --mum $INPUT_DIR/NC_011916.fna $INPUT_DIR/NC_002696.fna
+delta-filter -1 $DELTA_DIR/NC_011916_vs_NC_002696.delta > $FILTER_DIR/NC_011916_vs_NC_002696.filter
 
-nucmer -p $delta_dir/NC_011916_vs_NC_010338 --mum $input_dir/NC_011916.fna $input_dir/NC_010338.fna
-delta-filter -1 $delta_dir/NC_011916_vs_NC_010338.delta > $filter_dir/NC_011916_vs_NC_010338.filter
+nucmer -p $DELTA_DIR/NC_011916_vs_NC_010338 --mum $INPUT_DIR/NC_011916.fna $INPUT_DIR/NC_010338.fna
+delta-filter -1 $DELTA_DIR/NC_011916_vs_NC_010338.delta > $FILTER_DIR/NC_011916_vs_NC_010338.filter
 
-nucmer -p $delta_dir/NC_011916_vs_NC_014100 --mum $input_dir/NC_011916.fna $input_dir/NC_014100.fna
-delta-filter -1 $delta_dir/NC_011916_vs_NC_014100.delta > $filter_dir/NC_011916_vs_NC_014100.filter
+nucmer -p $DELTA_DIR/NC_011916_vs_NC_014100 --mum $INPUT_DIR/NC_011916.fna $INPUT_DIR/NC_014100.fna
+delta-filter -1 $DELTA_DIR/NC_011916_vs_NC_014100.delta > $FILTER_DIR/NC_011916_vs_NC_014100.filter
 
-nucmer -p $delta_dir/NC_014100_vs_NC_002696 --mum $input_dir/NC_014100.fna $input_dir/NC_002696.fna
-delta-filter -1 $delta_dir/NC_014100_vs_NC_002696.delta > $filter_dir/NC_014100_vs_NC_002696.filter
+nucmer -p $DELTA_DIR/NC_014100_vs_NC_002696 --mum $INPUT_DIR/NC_014100.fna $INPUT_DIR/NC_002696.fna
+delta-filter -1 $DELTA_DIR/NC_014100_vs_NC_002696.delta > $FILTER_DIR/NC_014100_vs_NC_002696.filter
 
-nucmer -p $delta_dir/NC_014100_vs_NC_010338 --mum $input_dir/NC_014100.fna $input_dir/NC_010338.fna
-delta-filter -1 $delta_dir/NC_014100_vs_NC_010338.delta > $filter_dir/NC_014100_vs_NC_010338.filter
+nucmer -p $DELTA_DIR/NC_014100_vs_NC_010338 --mum $INPUT_DIR/NC_014100.fna $INPUT_DIR/NC_010338.fna
+delta-filter -1 $DELTA_DIR/NC_014100_vs_NC_010338.delta > $FILTER_DIR/NC_014100_vs_NC_010338.filter
 
-nucmer -p $delta_dir/NC_014100_vs_NC_011916 --mum $input_dir/NC_014100.fna $input_dir/NC_011916.fna
-delta-filter -1 $delta_dir/NC_014100_vs_NC_011916.delta > $filter_dir/NC_014100_vs_NC_011916.filter
+nucmer -p $DELTA_DIR/NC_014100_vs_NC_011916 --mum $INPUT_DIR/NC_014100.fna $INPUT_DIR/NC_011916.fna
+delta-filter -1 $DELTA_DIR/NC_014100_vs_NC_011916.delta > $FILTER_DIR/NC_014100_vs_NC_011916.filter

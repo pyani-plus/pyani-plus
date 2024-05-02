5d1dd0f9ad3aa5c0bc1e7004f2ea46b0a8f4e714
kiepczi <angelika.kiepas@strath.ac.uk>
2024-05-02 11:44:50 +0100

    update format of fixture generation script
    - make shebang more portable
    - make variables more portable

--- tests/generate_fixtures/generate_target_fastani_files.sh
+++ tests/generate_fixtures/generate_target_fastani_files.sh
@@ -1,49 +1,38 @@
-#!/bin/bash
+#!/bin/env bash
 
 # This bash script generates target files for fastani comparisions.
 # Genomes are compared in both directions (forward and reverse)
 # using fastANI.
 
-# Paths to directories (eg, input sequences, fastANI outputs)
-input_dir=../fixtures/sequences
-fastani_dir=../fixtures/fastani/targets
-fragLen=3000
-kmerSize=16
-minFrac=0.2
+# Additional parameters (eg, input sequences, fastANI outputs, k-mer sizes...)
+INPUT_DIR=../fixtures/sequences
+FASTANI_DIR=../fixtures/fastani/targets
+FRAG_LEN=3000
+KMER_SIZE=16
+MIN_FRAC=0.2
 
 
 #Running comparisions
-fastANI -q $input_dir/NC_002696.fna -r $input_dir/NC_010338.fna -o $fastani_dir/NC_002696_vs_NC_010338.fastani --fragLen $fragLen -k $kmerSize --minFraction $minFrac
+fastANI -q $INPUT_DIR/NC_002696.fna -r $INPUT_DIR/NC_010338.fna -o $FASTANI_DIR/NC_002696_vs_NC_010338.fastani --fragLen $FRAG_LEN -k $KMER_SIZE --minFraction $MIN_FRAC
 
+fastANI -q $INPUT_DIR/NC_002696.fna -r $INPUT_DIR/NC_011916.fna -o $FASTANI_DIR/NC_002696_vs_NC_011916.fastani --fragLen $FRAG_LEN -k $KMER_SIZE --minFraction $MIN_FRAC
 
-fastANI -q $input_dir/NC_002696.fna -r $input_dir/NC_011916.fna -o $fastani_dir/NC_002696_vs_NC_011916.fastani --fragLen $fragLen -k $kmerSize --minFraction $minFrac
+fastANI -q $INPUT_DIR/NC_002696.fna -r $INPUT_DIR/NC_014100.fna -o $FASTANI_DIR/NC_002696_vs_NC_014100.fastani --fragLen $FRAG_LEN -k $KMER_SIZE --minFraction $MIN_FRAC
 
+fastANI -q $INPUT_DIR/NC_010338.fna -r $INPUT_DIR/NC_002696.fna -o $FASTANI_DIR/NC_010338_vs_NC_002696.fastani --fragLen $FRAG_LEN -k $KMER_SIZE --minFraction $MIN_FRAC
 
-fastANI -q $input_dir/NC_002696.fna -r $input_dir/NC_014100.fna -o $fastani_dir/NC_002696_vs_NC_014100.fastani --fragLen $fragLen -k $kmerSize --minFraction $minFrac
+fastANI -q $INPUT_DIR/NC_010338.fna -r $INPUT_DIR/NC_011916.fna -o $FASTANI_DIR/NC_010338_vs_NC_011916.fastani --fragLen $FRAG_LEN -k $KMER_SIZE --minFraction $MIN_FRAC
 
+fastANI -q $INPUT_DIR/NC_010338.fna -r $INPUT_DIR/NC_014100.fna -o $FASTANI_DIR/NC_010338_vs_NC_014100.fastani --fragLen $FRAG_LEN -k $KMER_SIZE --minFraction $MIN_FRAC
 
-fastANI -q $input_dir/NC_010338.fna -r $input_dir/NC_002696.fna -o $fastani_dir/NC_010338_vs_NC_002696.fastani --fragLen $fragLen -k $kmerSize --minFraction $minFrac
+fastANI -q $INPUT_DIR/NC_011916.fna -r $INPUT_DIR/NC_002696.fna -o $FASTANI_DIR/NC_011916_vs_NC_002696.fastani --fragLen $FRAG_LEN -k $KMER_SIZE --minFraction $MIN_FRAC
 
+fastANI -q $INPUT_DIR/NC_011916.fna -r $INPUT_DIR/NC_010338.fna -o $FASTANI_DIR/NC_011916_vs_NC_010338.fastani --fragLen $FRAG_LEN -k $KMER_SIZE --minFraction $MIN_FRAC
 
-fastANI -q $input_dir/NC_010338.fna -r $input_dir/NC_011916.fna -o $fastani_dir/NC_010338_vs_NC_011916.fastani --fragLen $fragLen -k $kmerSize --minFraction $minFrac
+fastANI -q $INPUT_DIR/NC_011916.fna -r $INPUT_DIR/NC_014100.fna -o $FASTANI_DIR/NC_011916_vs_NC_014100.fastani --fragLen $FRAG_LEN -k $KMER_SIZE --minFraction $MIN_FRAC
 
+fastANI -q $INPUT_DIR/NC_014100.fna -r $INPUT_DIR/NC_002696.fna -o $FASTANI_DIR/NC_014100_vs_NC_002696.fastani --fragLen $FRAG_LEN -k $KMER_SIZE --minFraction $MIN_FRAC
 
-fastANI -q $input_dir/NC_010338.fna -r $input_dir/NC_014100.fna -o $fastani_dir/NC_010338_vs_NC_014100.fastani --fragLen $fragLen -k $kmerSize --minFraction $minFrac
+fastANI -q $INPUT_DIR/NC_014100.fna -r $INPUT_DIR/NC_010338.fna -o $FASTANI_DIR/NC_014100_vs_NC_010338.fastani --fragLen $FRAG_LEN -k $KMER_SIZE --minFraction $MIN_FRAC
 
-
-fastANI -q $input_dir/NC_011916.fna -r $input_dir/NC_002696.fna -o $fastani_dir/NC_011916_vs_NC_002696.fastani --fragLen $fragLen -k $kmerSize --minFraction $minFrac
-
-
-fastANI -q $input_dir/NC_011916.fna -r $input_dir/NC_010338.fna -o $fastani_dir/NC_011916_vs_NC_010338.fastani --fragLen $fragLen -k $kmerSize --minFraction $minFrac
-
-
-fastANI -q $input_dir/NC_011916.fna -r $input_dir/NC_014100.fna -o $fastani_dir/NC_011916_vs_NC_014100.fastani --fragLen $fragLen -k $kmerSize --minFraction $minFrac
-
-
-fastANI -q $input_dir/NC_014100.fna -r $input_dir/NC_002696.fna -o $fastani_dir/NC_014100_vs_NC_002696.fastani --fragLen $fragLen -k $kmerSize --minFraction $minFrac
-
-
-fastANI -q $input_dir/NC_014100.fna -r $input_dir/NC_010338.fna -o $fastani_dir/NC_014100_vs_NC_010338.fastani --fragLen $fragLen -k $kmerSize --minFraction $minFrac
-
-
-fastANI -q $input_dir/NC_014100.fna -r $input_dir/NC_011916.fna -o $fastani_dir/NC_014100_vs_NC_011916.fastani --fragLen $fragLen -k $kmerSize --minFraction $minFrac
+fastANI -q $INPUT_DIR/NC_014100.fna -r $INPUT_DIR/NC_011916.fna -o $FASTANI_DIR/NC_014100_vs_NC_011916.fastani --fragLen $FRAG_LEN -k $KMER_SIZE --minFraction $MIN_FRAC

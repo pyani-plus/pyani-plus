713a4e54d03dc134e26642e77b020923e3db5d51
Peter Cock <p.j.a.cock@googlemail.com>
2024-09-25 15:18:10 +0100

    A narrower regex is safer here (for .fna, .fa, .fasta)

--- tests/generate_fixtures/generate_target_anim_files.py
+++ tests/generate_fixtures/generate_target_anim_files.py
@@ -48,8 +48,8 @@ DELTA_DIR = Path("../fixtures/anim/targets/delta")
 FILTER_DIR = Path("../fixtures/anim/targets/filter")
 
 # Running ANIm comparisons
-comparisons = permutations([_.stem for _ in Path(INPUT_DIR).glob("*")], 2)
-inputs = {_.stem: _ for _ in Path(INPUT_DIR).glob("*")}
+comparisons = permutations([_.stem for _ in Path(INPUT_DIR).glob("*.f*")], 2)
+inputs = {_.stem: _ for _ in Path(INPUT_DIR).glob("*.f*")}
 
 nucmer = get_nucmer()
 delta_filter = get_delta_filter()

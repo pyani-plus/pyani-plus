1e0069df5154e59803aefe94860ce6881f3ebe8c
Peter Cock <p.j.a.cock@googlemail.com>
2024-10-08 13:51:07 +0100

    Avoid repetition of the license block
    See discussion on #77, we may need to modify the license check?

--- pyani_plus/methods/method_anim.py
+++ pyani_plus/methods/method_anim.py
@@ -1,28 +1,11 @@
-# The MIT License
-#
-# Copyright (c) 2024 University of Strathclyde
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-# The MIT License
-#
+
+# Based on MIT licensed code from pyANI,
 # Copyright (c) 2016-2019 The James Hutton Institute
-# Copyright (c) 2019-present University of Strathclyde
+# Copyright (c) 2019-2024 University of Strathclyde
+#
+# The MIT License
+#
+# Copyright (c) 2069-2024 University of Strathclyde
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -41,6 +24,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+#
 """Code to implement the ANIm average nucleotide identity method.
 
 Calculates ANI by the ANIm method, as described in Richter et al (2009)

17685f9e92bd431cd5629594a62a09214c8117b2
kiepczi <angelika.kiepas@strath.ac.uk>
2024-11-19 18:33:24 +0000

    single rule for dnadiff method

--- tests/snakemake/test_snakemake_dnadiff_workflow.py
+++ tests/snakemake/test_snakemake_dnadiff_workflow.py
@@ -102,90 +102,6 @@ def compare_show_diff_files(file1: Path, file2: Path) -> bool:
     return True
 
 
-def test_snakemake_rule_delta(
-    dnadiff_nucmer_targets_delta: list[str],
-    dnadiff_nucmer_targets_delta_indir: Path,
-    dnadiff_nucmer_targets_delta_outdir: Path,
-    config_dnadiff_args: dict,
-    tmp_path: str,
-) -> None:
-    """Test nucmer delta snakemake wrapper.
-
-    Checks that the delta rule in the dnadiff snakemake wrapper gives the
-    expected output.
-
-    If the output directory exists (i.e. the make clean_tests rule has not
-    been run), the tests will automatically pass as snakemake will not
-    attempt to re-run the rule. That would prevent us from seeing any
-    introduced bugs, so we force re-running the rule by deleting the
-    output directory before running the tests.
-    """
-    # Remove the output directory to force re-running the snakemake rule
-    shutil.rmtree(dnadiff_nucmer_targets_delta_outdir, ignore_errors=True)
-
-    config = config_dnadiff_args.copy()
-    config["outdir"] = dnadiff_nucmer_targets_delta_outdir
-
-    # Run snakemake wrapper
-    run_snakemake_with_progress_bar(
-        executor=ToolExecutor.local,
-        workflow_name="snakemake_dnadiff.smk",
-        targets=dnadiff_nucmer_targets_delta,
-        params=config,
-        working_directory=Path(tmp_path),
-        show_progress_bar=False,
-    )
-
-    # Check output against target fixtures
-    for fname in dnadiff_nucmer_targets_delta:
-        assert compare_files_with_skip(
-            dnadiff_nucmer_targets_delta_indir / fname,
-            dnadiff_nucmer_targets_delta_outdir / fname,
-        )
-
-
-def test_snakemake_rule_filter(
-    dnadiff_nucmer_targets_filter: list[str],
-    dnadiff_nucmer_targets_filter_indir: Path,
-    dnadiff_nucmer_targets_filter_outdir: Path,
-    config_dnadiff_args: dict,
-    tmp_path: str,
-) -> None:
-    """Test nucmer filter snakemake wrapper.
-
-    Checks that the filter rule in the dnadiff snakemake wrapper gives the
-    expected output.
-
-    If the output directory exists (i.e. the make clean_tests rule has not
-    been run), the tests will automatically pass as snakemake will not
-    attempt to re-run the rule. That would prevent us from seeing any
-    introduced bugs, so we force re-running the rule by deleting the
-    output directory before running the tests.
-    """
-    # Remove the output directory to force re-running the snakemake rule
-    shutil.rmtree(dnadiff_nucmer_targets_filter_outdir, ignore_errors=True)
-
-    config = config_dnadiff_args.copy()
-    config["outdir"] = dnadiff_nucmer_targets_filter_outdir
-
-    # Run snakemake wrapper
-    run_snakemake_with_progress_bar(
-        executor=ToolExecutor.local,
-        workflow_name="snakemake_dnadiff.smk",
-        targets=dnadiff_nucmer_targets_filter,
-        params=config,
-        working_directory=Path(tmp_path),
-        show_progress_bar=False,
-    )
-
-    # Check output against target fixtures
-    for fname in dnadiff_nucmer_targets_filter:
-        assert compare_files_with_skip(
-            dnadiff_nucmer_targets_filter_indir / fname,
-            dnadiff_nucmer_targets_filter_outdir / fname,
-        )
-
-
 def test_snakemake_rule_show_diff_and_coords(  # noqa: PLR0913
     capsys: pytest.CaptureFixture[str],
     dnadiff_targets_showdiff: list[str],
@@ -256,7 +172,7 @@ def test_snakemake_rule_show_diff_and_coords(  # noqa: PLR0913
     compare_matrices(db, dir_dnadiff_matrices, absolute_tolerance=5e-5)
 
 
-def test_snakemake_rule_show_coords(  # noqa: PLR0913
+def test_dnadiff(  # noqa: PLR0913
     dnadiff_targets_showcoords: list[str],
     dnadiff_targets_showcoords_indir: Path,
     dnadiff_targets_showcoords_outdir: Path,
@@ -264,7 +180,7 @@ def test_snakemake_rule_show_coords(  # noqa: PLR0913
     dir_dnadiff_matrices: Path,
     tmp_path: str,
 ) -> None:
-    """Test dnadiff show-coords snakemake wrapper.
+    """Test rule dnadiff.
 
     Checks that the show_diff_and_coords rule in the dnadiff snakemake wrapper gives the
     expected show-coords output.

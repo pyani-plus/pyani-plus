885d1ad96fec35c60cd2ce5f272922e88057372f
kiepczi <angelika.kiepas@strath.ac.uk>
2024-11-19 12:00:16 +0000

    single smk rule for aniM

--- tests/snakemake/test_snakemake_anim_workflow.py
+++ tests/snakemake/test_snakemake_anim_workflow.py
@@ -85,7 +85,7 @@ def compare_files_with_skip(file1: Path, file2: Path, skip: int = 1) -> bool:
     return True
 
 
-def test_snakemake_rule_filter(  # noqa: PLR0913
+def test_snakemake_rule_ANIm(  # noqa: N802, PLR0913
     anim_nucmer_targets_filter: list[str],
     anim_nucmer_targets_filter_indir: Path,
     anim_nucmer_targets_filter_outdir: Path,
@@ -149,45 +149,3 @@ def test_snakemake_rule_filter(  # noqa: PLR0913
             anim_nucmer_targets_filter_outdir / fname,
         )
     compare_matrices(db, dir_anim_results)
-
-
-def test_snakemake_rule_delta(
-    anim_nucmer_targets_delta: list[str],
-    anim_nucmer_targets_delta_indir: Path,
-    anim_nucmer_targets_delta_outdir: Path,
-    config_anim_args: dict,
-    tmp_path: str,
-) -> None:
-    """Test nucmer delta snakemake wrapper.
-
-    Checks that the delta rule in the ANIm snakemake wrapper gives the
-    expected output.
-
-    If the output directory exists (i.e. the make clean_tests rule has not
-    been run), the tests will automatically pass as snakemake will not
-    attempt to re-run the rule. That would prevent us from seeing any
-    introduced bugs, so we force re-running the rule by deleting the
-    output directory before running the tests.
-    """
-    # Remove the output directory to force re-running the snakemake rule
-    shutil.rmtree(anim_nucmer_targets_delta_outdir, ignore_errors=True)
-
-    config = config_anim_args.copy()
-    config["outdir"] = anim_nucmer_targets_delta_outdir
-
-    # Run snakemake wrapper
-    run_snakemake_with_progress_bar(
-        executor=ToolExecutor.local,
-        workflow_name="snakemake_anim.smk",
-        targets=anim_nucmer_targets_delta,
-        params=config,
-        working_directory=Path(tmp_path),
-        show_progress_bar=False,
-    )
-
-    # Check output against target fixtures
-    for fname in anim_nucmer_targets_delta:
-        assert compare_files_with_skip(
-            anim_nucmer_targets_delta_indir / fname,
-            anim_nucmer_targets_delta_outdir / fname,
-        )

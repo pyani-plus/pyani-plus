11a819cb658b82e1c5b7d532f3219f60b5bbdf1c
Peter J. A. Cock <p.j.a.cock@googlemail.com>
2024-12-10 23:18:55 +0000

    Transpose sourmash.csv to match our convention
    This doesn't matter with the symmetric --max-containment
    mode (which was our default), only with --containment
    (which we now map to our query-coverage field).

--- tests/generate_fixtures/generate_target_sourmash_matrices.py
+++ tests/generate_fixtures/generate_target_sourmash_matrices.py
@@ -57,7 +57,25 @@ def parse_compare_files(compare_file: Path) -> float:
 matrix = pd.read_csv(INPUT_DIR / "intermediates/sourmash/sourmash.csv")
 matrix.columns = [Path(_).stem for _ in matrix]
 matrix.index = matrix.columns
-matrix = matrix.sort_index(axis=0).sort_index(axis=1)
+matrix = matrix.sort_index(axis=0).sort_index(axis=1).T
+# Note we transpose sourmash.csv compared to our own matrices!
+#
+# Quoting https://sourmash.readthedocs.io/en/latest/command-line.html#sourmash-compare-compare-many-signatures
+#
+#   Note: compare by default produces a symmetric similarity matrix that can be
+#   used for clustering in downstream tasks. With --containment, however, this
+#   matrix is no longer symmetric and cannot formally be used for clustering.
+#
+#   The containment matrix is organized such that the value in row A for column B
+#   is the containment of the B'th sketch in the A'th sketch, i.e.
+#
+#   C(A, B) = B.contained_by(A)
+#
+# See also the sourmash branchwater output which has explicit query and subject
+# columns, and query_containment and subject_containment.
+#
+# I interpret this as column B is the query, row A is the subject - which is the
+# transpose of the convention we are using.
 
 # If ANI values can't be estimated (eg. sourmash 0.0) report None instead
 matrix = matrix.replace(0.0, None)

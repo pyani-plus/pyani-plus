a04b8a334654f574a8595649779e5c71224ff182
Peter Cock <p.j.a.cock@googlemail.com>
2025-01-07 16:14:14 +0000

    Fix typo on test docstring
    [ci skip]

--- tests/test_public_cli.py
+++ tests/test_public_cli.py
@@ -19,7 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-"""Tests for the pyani_plus/db_orm.py module.
+"""Tests for the pyani_plus/public_cli.py module.
 
 These tests are intended to be run from the repository root using:
 

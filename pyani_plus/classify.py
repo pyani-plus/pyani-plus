f308a62bd969c33a734168ac4cfb1fae2b67abeb
kiepczi <angelika.kiepas@strath.ac.uk>
2025-01-23 10:09:24 +0000

    add classify module to find cliques

---
+++ pyani_plus/classify.py
@@ -0,0 +1,186 @@
+# The MIT License
+#
+# Copyright (c) 2025 University of Strathclyde
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""Code to implement the classify method indetnded to identify cliques within a set of genomes."""
+
+from collections.abc import Callable
+from itertools import combinations
+from pathlib import Path
+from typing import NamedTuple
+
+import networkx as nx
+import numpy as np
+import pandas as pd
+
+AGG_FUNCS = {
+    "min": min,
+    "max": max,
+    "mean": np.mean,
+}
+
+MIN_COVERAGE = 0.50
+
+
+class CliqueInfo(NamedTuple):
+    """Graph structure summary."""
+
+    members: list
+    n_nodes: int
+    min_cov: float | None
+    min_identity: float | None
+
+
+def construct_graph(
+    cov_matrix: pd.DataFrame,
+    id_matrix: pd.DataFrame,
+    coverage_agg: Callable,
+    identity_agg: Callable,
+    min_coverage: float,
+) -> nx.Graph:
+    """Return a graph representing ANI results.
+
+    Constructs an undirected graph for the ANI results of a given run_id.
+    Nodes represent genome, and edges correspond to pairwise comparisons,
+    with weights assigned based on the minimum coverage and the average identity
+    (by default). Edges are not added between genome pairs if no alignment is
+    found or if the coverage is below 50%.
+    """
+    # Create an empty graph
+    graph = nx.Graph()
+
+    # Get list of nodes for a graph (eg. genome hashes) and a list of comparisons (eg. genome A vs genome B)
+    nodes = cov_matrix.columns
+    graph.add_nodes_from(nodes)
+    comparisons = list(combinations(nodes, 2))
+
+    # Add edges to the graph based on ANI results. Since the methods are not symmetrical
+    # (e.g., comparisons between genome A and genome B are expected to return slightly different
+    # values compared to those between genome B and genome A), we loop over the comparisons
+    # and check the results in both directions. For each comparison, by default, we use
+    # the lowest genome coverage and the average genome identity to create the graph.
+    # However, we allow the end-user to choose alternatives, such as min or max. No edges
+    # are added if no alignment is found.
+    for genome1, genome2 in comparisons:
+        coverage = coverage_agg(
+            [cov_matrix[genome1][genome2], cov_matrix[genome2][genome1]]
+        )
+        identity = identity_agg(
+            [id_matrix[genome1][genome2], id_matrix[genome2][genome1]]
+        )
+        # Add edge only if both coverage and identity are valid
+        if pd.notna(coverage) and pd.notna(identity) and coverage > min_coverage:
+            graph.add_edge(genome1, genome2, coverage=coverage, identity=identity)
+
+    return graph
+
+
+def is_clique(graph: nx.Graph) -> bool:
+    """Return True if the subgraph is a clique."""
+    n_nodes = len(graph.nodes)
+    return len(graph.edges) == n_nodes * (n_nodes - 1) / 2
+
+
+def flatten(list_of_lists: list) -> list:
+    """Flatten a list of lists into a single list."""
+    return [item for sublist in list_of_lists for item in sublist]
+
+
+def find_initial_cliques(graph: nx.Graph) -> list:
+    """Return all unique cliques in the given graph.
+
+    Since the initial graph has edges removed below the 50% coverage
+    threshold (by default), it is possible that the graph contains
+    subgraphs that are potential cliques, which we may want to identify
+    before removing any edges.
+    """
+    cliques: list(nx.Graph) = []  # type: ignore  # noqa: PGH003
+    connected_components = list(nx.connected_components(graph))
+    for component in connected_components:
+        subgraph = graph.subgraph(component).copy()
+        if (
+            is_clique(subgraph) and len(subgraph) != 1
+        ):  # Check if the subgraph is a clique
+            cliques.append(subgraph)
+
+    return cliques
+
+
+def find_cliques_recursively(graph: nx.Graph) -> list:
+    """Return all unique cliques within a set of genomes based on ANI results.
+
+    These cliques are identified recursively by iteratively removing edges
+    with the lowest identity at each step and analysing the resulting subgraphs.
+    """
+    cliques: list(nx.Graph) = []  # type: ignore  # noqa: PGH003
+
+    # If the graph has only one node stop recursion and return list of cliques
+    if len(graph.nodes) == 1:
+        cliques.append(graph)
+        return cliques
+    # Recording cliques
+    if is_clique(graph):
+        cliques.append(graph.copy())
+
+    # Remove edges with the lowest weight and identify cliques
+    edges = list(nx.get_edge_attributes(graph, "identity"))
+    while edges:
+        edge_to_remove = edges.pop(0)
+        graph.remove_edge(edge_to_remove[0], edge_to_remove[1])
+
+        connected_components = list(nx.connected_components(graph))
+        if len(connected_components) > 1:
+            new_cliques = flatten(
+                [
+                    find_cliques_recursively(graph.subgraph(component).copy())
+                    for component in connected_components
+                ]
+            )
+            return cliques + new_cliques
+    return cliques
+
+
+def compute_classify_output(
+    cliques: list, method: str, outdir: Path
+) -> list[CliqueInfo]:
+    """Return list of CliqueInfo describing all cliques found and save them to .tsv file."""
+    clique_data = [
+        CliqueInfo(
+            members=list(clique.nodes),
+            n_nodes=len(clique.nodes),
+            min_cov=min(
+                (attrs["coverage"] for _, _, attrs in clique.edges(data=True)),
+                default=None,
+            ),
+            min_identity=min(
+                (attrs["identity"] for _, _, attrs in clique.edges(data=True)),
+                default=None,
+            ),
+        )
+        for clique in cliques
+    ]
+
+    clique_df = pd.DataFrame(clique_data)
+    clique_df["members"] = clique_df["members"].apply(lambda x: ",".join(x))
+
+    output_file = outdir / f"{method}_classify.tsv"
+    clique_df.to_csv(output_file, sep="\t", index=False)
+
+    return clique_data
---
+++ tests/test_classify.py
@@ -0,0 +1,192 @@
+# The MIT License
+#
+# Copyright (c) 2025 University of Strathclyde
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""Tests for the classify implementation.
+
+These tests are intended to be run from the repository root using:
+
+pytest -v
+"""
+
+import networkx as nx  # type: ignore  # noqa: PGH003
+import networkx.algorithms.isomorphism as iso
+import numpy as np
+import pandas as pd
+import pytest
+
+from pyani_plus import classify
+
+
+@pytest.fixture
+def known_graph_with_dataframes() -> tuple[nx.Graph, pd.DataFrame, pd.DataFrame]:
+    """Return nx.Graph with known weights (coverage and identity) and corresponding coverage and identity DataFrames.
+
+    The graph:
+    - Contains 5 nodes named 'genome_1' to 'genome_5'.
+    - Each pair of nodes is connected by an edge with the following attributes:
+        - 'coverage': Fixed at 1.0 for all edges.
+        - 'identity': Starts at 0.80 and increases by 0.01 for each subsequent edge.
+    - Edges are added in a deterministic order such that the identity values remain consistent
+      across multiple runs.
+    """
+    # Create an empty graph and add nodes
+    graph = nx.Graph()
+    nodes = [f"genome_{i}" for i in range(1, 6)]
+    graph.add_nodes_from(nodes)
+
+    # Create empty DataFrames
+    coverage_df = pd.DataFrame(None, index=nodes, columns=nodes)
+    identity_df = pd.DataFrame(None, index=nodes, columns=nodes)
+
+    # Add edges with attributes
+    identity_value = 0.80
+    edges = []
+    for i, node1 in enumerate(nodes):
+        for node2 in nodes[i + 1 :]:
+            # Add edges to the graph
+            edge_attributes = {"coverage": 1.0, "identity": round(identity_value, 2)}
+            edges.append((node1, node2, edge_attributes))
+
+            # Update DataFrames ()
+            coverage_df.loc[node1, node2] = 1.0
+            coverage_df.loc[node2, node1] = 1.0
+            identity_df.loc[node1, node2] = round(identity_value, 2)
+            identity_df.loc[node2, node1] = round(identity_value, 2)
+
+            # Increment identity value
+            identity_value += 0.01
+
+    graph.add_edges_from(edges)
+
+    return graph, coverage_df, identity_df
+
+
+@pytest.fixture
+def expected_cliques(
+    known_graph_with_dataframes: tuple[nx.Graph, pd.DataFrame, pd.DataFrame],
+) -> list:
+    """Generate a list of expected cliques based on the known graph.
+
+    Since, the known graph is constructed such that nodes are connected in order,
+    and edge 'identity' values increment by 0.01, starting at 0.80. Removing
+    'genome_1' first results in the removal of edges with the lowest identity
+    values, leaving behind the first expected clique. We can generate a list
+    of all possible cliques that should be identified by the classification method
+    """
+    # Copy the original graph to avoid modifying the fixture
+    original_graph = known_graph_with_dataframes[0].copy()
+    graph = original_graph.copy()
+
+    # Define empty list to which we will append known cliques
+    known_cliques = []
+
+    # Iterate through nodes in the graph
+    for node in list(graph.nodes):
+        if node != "genome_5":
+            # Add single-node graph as a clique
+            single_node_clique = nx.Graph()
+            single_node_clique.add_node(node)
+            known_cliques.append(single_node_clique)
+
+            # Remove the node and add the resulting graph as a clique
+            graph.remove_node(node)
+            known_cliques.append(graph.copy())  # Copy to preserve graph state
+
+    # Include the initial graph as the first clique
+    return [original_graph, *known_cliques]
+
+
+def test_construct_graph(
+    known_graph_with_dataframes: tuple[nx.Graph, pd.DataFrame, pd.DataFrame],
+) -> None:
+    """Check construction of the initial graph."""
+    graph, coverage_df, identity_df = known_graph_with_dataframes
+
+    # Comparison function for a numerical edge attribute.
+    edge_match = iso.numerical_edge_match("coverage", "identity")
+
+    # Check the isomorphism of a graph with the edge_match function
+    assert nx.is_isomorphic(
+        graph,
+        classify.construct_graph(coverage_df, identity_df, min, np.mean, 0.5),
+        edge_match=edge_match,
+    )
+
+
+def test_is_clique(
+    known_graph_with_dataframes: tuple[nx.Graph, pd.DataFrame, pd.DataFrame],
+) -> None:
+    """Check cliques are identified."""
+    graph = known_graph_with_dataframes[0]
+    assert classify.is_clique(graph) is True
+
+
+def test_flatten_list() -> None:
+    """Check if lists of lists are flattened correctly."""
+    list_of_lists = [[1], [2], [3, 4], [5, 6]]
+    expected = [1, 2, 3, 4, 5, 6]
+
+    assert expected == classify.flatten(list_of_lists)
+
+
+def test_find_initial_cliques(
+    known_graph_with_dataframes: tuple[nx.Graph, pd.DataFrame, pd.DataFrame],
+) -> None:
+    """Check all possible cliques are identified in the initial iteration."""
+    graph = known_graph_with_dataframes[0]
+    found_cliques = classify.find_initial_cliques(graph)
+    edge_match = iso.numerical_edge_match("coverage", "identity")
+
+    # Get the connected components as subgraphs
+    connected_components = [
+        graph.subgraph(component).copy() for component in nx.connected_components(graph)
+    ]
+
+    # Check the number of identified cliques
+    assert len(connected_components) == len(found_cliques), "Clique count mismatch"
+
+    # Check the clique structures
+    for expected_clique, found_clique in zip(
+        connected_components, found_cliques, strict=False
+    ):
+        assert nx.is_isomorphic(expected_clique, found_clique, edge_match=edge_match), (
+            "Clique structure mismatch"
+        )
+
+
+def test_find_cliques(
+    known_graph_with_dataframes: tuple[nx.Graph, pd.DataFrame, pd.DataFrame],
+    expected_cliques: list,
+) -> None:
+    """Check all possible cliques are identified in the initial iteration."""
+    graph = known_graph_with_dataframes[0]
+    found_cliques = classify.find_cliques_recursively(graph)
+    edge_match = iso.numerical_edge_match("coverage", "identity")
+
+    # Check the number of identified cliques
+    assert len(found_cliques) == len(expected_cliques), "Clique count mismatch"
+
+    for expected_clique, found_clique in zip(
+        expected_cliques, found_cliques, strict=False
+    ):
+        assert nx.is_isomorphic(expected_clique, found_clique, edge_match=edge_match), (
+            "Clique structure mismatch"
+        )

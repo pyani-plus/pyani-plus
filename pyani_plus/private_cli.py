e362d61531f9eeac92a925ba13848f6683e53453
Peter J. A. Cock <p.j.a.cock@googlemail.com>
2025-02-26 10:23:49 +0000

    Not realistic to test interupting sourmash and logging partial progress

--- pyani_plus/private_cli.py
+++ pyani_plus/private_cli.py
@@ -1297,11 +1297,11 @@ def compute_sourmash(  # noqa: PLR0913
                     "uname_machine": uname_machine,
                 }
             )
-    except KeyboardInterrupt:
+    except KeyboardInterrupt:  # pragma: no cover
         # Try to abort gracefully without wasting the work done.
-        msg = f"Interrupted, will attempt to log {len(db_entries)} completed comparisons\n"
-        sys.stderr.write(msg)
-        run.status = "Worker interrupted"
+        msg = f"Interrupted, will attempt to log {len(db_entries)} completed comparisons\n"  # pragma: no cover
+        sys.stderr.write(msg)  # pragma: no cover
+        run.status = "Worker interrupted"  # pragma: no cover
     return (
         0
         if db_orm.insert_comparisons_with_retries(session, db_entries)

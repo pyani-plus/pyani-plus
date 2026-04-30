57d341197938db8ccb9bf5e4a9f0f7408ace8cbb
Peter J. A. Cock <p.j.a.cock@googlemail.com>
2026-04-30 19:50:53 +0100

    Test missing ANIminimap2 preset

--- tests/test_animinimap2.py
+++ tests/test_animinimap2.py
@@ -108,3 +108,52 @@ def test_running_animinimap2(
         query_hashes=hash_to_lengths,
         subject_hash=list(hash_to_filename)[1],
     )
+
+
+def test_skani_missing_preset(
+    tmp_path: str,
+    input_genomes_tiny: Path,
+) -> None:
+    """Check that ANIminimap2 fails if preset argument missing."""
+    tmp_dir = Path(tmp_path)
+    tmp_db = tmp_dir / "new.sqlite"
+    assert not tmp_db.is_file()
+    tmp_json = tmp_dir / "minimap2.json"
+
+    tool = tools.get_minimap2()
+
+    private_cli.log_run(
+        fasta=input_genomes_tiny,
+        database=tmp_db,
+        cmdline="pyani-plus animinimap2 ...",
+        status="Testing",
+        name="Testing missing preset",
+        method="ANIminimap2",
+        program=tool.exe_path.stem,
+        version=tool.version,
+        create_db=True,
+    )
+
+    logger = setup_logger(None)
+    session = db_orm.connect_to_db(logger, tmp_db)
+    run = session.query(db_orm.Run).one()
+    assert run.run_id == 1
+    filename_to_hash = {_.fasta_filename: _.genome_hash for _ in run.fasta_hashes}
+    hash_to_filename = {_.genome_hash: _.fasta_filename for _ in run.fasta_hashes}
+    hash_to_lengths = {_.genome_hash: _.length for _ in run.genomes}
+
+    with pytest.raises(
+        SystemExit, match="ANIminimap2 run-id 1 is missing preset parameter"
+    ):
+        private_cli.compute_animinimap2(
+            logger,
+            tmp_dir,
+            session,
+            run,
+            tmp_json,
+            input_genomes_tiny,
+            hash_to_filename,
+            filename_to_hash,
+            query_hashes=hash_to_lengths,
+            subject_hash=list(hash_to_filename)[1],
+        )

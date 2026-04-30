168b828855bb9b36cc1e244d8eb3f34935371aea
Leighton Pritchard <leightonpritchard@mac.com>
2026-04-30 17:29:11 +0100

    use context management for database sessions
    Final cherry-pick for context management. This commit had included
    some new lz-ani-related code that has had to be removed.

--- tests/test_dnadiff.py
+++ tests/test_dnadiff.py
@@ -114,52 +114,52 @@ def test_running_dnadiff(
     )
 
     logger = setup_logger(None)
-    session = db_orm.connect_to_db(logger, tmp_db)
-    run = session.query(db_orm.Run).one()
-    assert run.run_id == 1
-    hash_to_filename = {_.genome_hash: _.fasta_filename for _ in run.fasta_hashes}
-    hash_to_length = {_.genome_hash: _.length for _ in run.genomes}
+    with db_orm.connect_to_db(logger, tmp_db) as session:
+        run = session.query(db_orm.Run).one()
+        assert run.run_id == 1
+        hash_to_filename = {_.genome_hash: _.fasta_filename for _ in run.fasta_hashes}
+        hash_to_length = {_.genome_hash: _.length for _ in run.genomes}
 
-    subject_hash = list(hash_to_filename)[1]
-    private_cli.compute_dnadiff(
-        logger,
-        tmp_dir,
-        session,
-        run,
-        tmp_json,
-        input_genomes_tiny,
-        hash_to_filename,
-        {},  # not used for dnadiff
-        query_hashes=hash_to_length,  # order should not matter!
-        subject_hash=subject_hash,
-    )
-    assert tmp_json.is_file()
-
-    private_cli.import_json_comparisons(logger, session, tmp_json)
-
-    assert session.query(db_orm.Comparison).count() == 3  # noqa: PLR2004
-    assert (
-        session.query(db_orm.Comparison)
-        .where(db_orm.Comparison.subject_hash == subject_hash)
-        .count()
-        == 3  # noqa: PLR2004
-    )
-
-    # Check the intermediate files match?
-
-    # No real need to test the ANI values here, will be done elsewhere.
-    for query_hash, query_filename in hash_to_filename.items():
-        pytest.approx(
-            get_matrix_entry(
-                input_genomes_tiny / "matrices/dnadiff_identity.tsv",
-                Path(query_filename).stem,
-                Path(hash_to_filename[subject_hash]).stem,
-            )
-            == session.query(db_orm.Comparison)
-            .where(db_orm.Comparison.query_hash == query_hash)
-            .where(db_orm.Comparison.subject_hash == subject_hash)
-            .one()
-            .identity
+        subject_hash = list(hash_to_filename)[1]
+        private_cli.compute_dnadiff(
+            logger,
+            tmp_dir,
+            session,
+            run,
+            tmp_json,
+            input_genomes_tiny,
+            hash_to_filename,
+            {},  # not used for dnadiff
+            query_hashes=hash_to_length,  # order should not matter!
+            subject_hash=subject_hash,
         )
-    session.close()
+        assert tmp_json.is_file()
+
+        private_cli.import_json_comparisons(logger, session, tmp_json)
+
+        assert session.query(db_orm.Comparison).count() == 3  # noqa: PLR2004
+        assert (
+            session.query(db_orm.Comparison)
+            .where(db_orm.Comparison.subject_hash == subject_hash)
+            .count()
+            == 3  # noqa: PLR2004
+        )
+
+        # Check the intermediate files match?
+
+        # No real need to test the ANI values here, will be done elsewhere.
+        for query_hash, query_filename in hash_to_filename.items():
+            pytest.approx(
+                get_matrix_entry(
+                    input_genomes_tiny / "matrices/dnadiff_identity.tsv",
+                    Path(query_filename).stem,
+                    Path(hash_to_filename[subject_hash]).stem,
+                )
+                == session.query(db_orm.Comparison)
+                .where(db_orm.Comparison.query_hash == query_hash)
+                .where(db_orm.Comparison.subject_hash == subject_hash)
+                .one()
+                .identity
+            )
+
     tmp_db.unlink()
